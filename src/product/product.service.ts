2c017408b5fbeeb83832790f65dcb5d20a63041b
Mateus Guedes <mxteusgued@gmail.com>
2024-11-15 16:32:33 -0300

    feat: create product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService) {}
+
+  create(data: Prisma.ProductCreateInput) {
+    return this.prisma.product.create({ data });
   }
 
   findAll() {
