b7dd6cac7bd160de67591af07899ff0150e4df0b
Mateus Guedes <mxteusgued@gmail.com>
2024-11-17 19:10:58 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Z1 Ecommerce')
+    .setDescription('The Z1 Ecommerce API description')
+    .setVersion('1.0')
+    .addTag('orders')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
